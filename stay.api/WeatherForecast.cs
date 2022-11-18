8550db8533b0dfce1cce88c8c888458a50fc7d93
Samy Mokhtari <samy.mokhtari213@gmail.com>
2022-11-18 23:15:43 +0100

    implement auth service consumation
    fix ci
    
    fix ci
    
    ignore obj,bin,release,debug
    
    delete unwanted directories
    
    clean directories

--- stay.api/WeatherForecast.cs
+++ stay.api/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace stay.api
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public object User { get; set; }
     }
 }
